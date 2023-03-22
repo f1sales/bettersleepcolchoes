f63972784e274309434fd15a8f3dd62546ecc37f
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2023-03-22 12:06:12 -0300

    F1API-686: Add filter store

--- lib/bettersleepcolchoes.rb
+++ lib/bettersleepcolchoes.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 
 require_relative 'bettersleepcolchoes/version'
+require 'f1sales_custom/parser'
+require 'f1sales_custom/source'
+require 'f1sales_custom/hooks'
 
 module Bettersleepcolchoes
   class Error < StandardError; end
-  # Your code goes here...
+
+  class F1SalesCustom::Hooks::Lead
+    def self.switch_source(lead)
+      source_name = lead.source.name
+      description = lead.description
+
+      source_name = "#{source_name} - #{description}" if source_name['Simmons']
+
+      source_name
+    end
+  end
 end
---
+++ spec/hooks_spec.rb
@@ -0,0 +1,54 @@
+require 'ostruct'
+
+RSpec.describe F1SalesCustom::Hooks::Lead do
+  context 'when come from Simmons' do
+    let(:lead) do
+      lead = OpenStruct.new
+      lead.source = source
+      lead.description = ''
+
+      lead
+    end
+
+    let(:source) do
+      source = OpenStruct.new
+      source.name = 'Fonte'
+
+      source
+    end
+
+    let(:switch_source) { described_class.switch_source(lead) }
+
+    context 'when is not form Simmons' do
+      it 'returns source' do
+        expect(switch_source).to eq('Fonte')
+      end
+    end
+
+    context 'when leads come from Simmons - Widgrid ' do
+      before do
+        source.name = 'Simmons - Widgrid'
+        lead.description = 'Better Sleep Aldeota'
+      end
+
+      context 'when lead is to Better Sleep Aldeota' do
+        it 'returns Simmons - Widgrid - Better Sleep Aldeota' do
+          expect(switch_source).to eq('Simmons - Widgrid - Better Sleep Aldeota')
+        end
+      end
+    end
+
+    context 'when leads come form Simmons - Facebook' do
+      before do
+        source.name = 'Simmons - Facebook'
+        lead.description = 'Better Sleep Antonio Sales'
+      end
+
+      context 'when lead is to Better Sleep Aldeota' do
+        it 'returns Simmons - Facebook - Better Sleep Aldeota' do
+          expect(switch_source).to eq('Simmons - Facebook - Better Sleep Antonio Sales')
+        end
+      end
+    end
+  end
+end
